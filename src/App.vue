3bcd611ad0068f60c0cd82ed459555dab11e7374
Wojciech Frącz <fraczwojciech@gmail.com>
2020-04-24 15:06:12 +0200

    Button label and many login forms

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,10 @@
         </div>
 
         <div v-else>
-            <login-form @login="logMeIn($event)"></login-form>
+            <login-form @login="logMeIn($event)" button-label="Wejdź"></login-form>
+            <login-form @login="logMeIn($event)" button-label="Wleć"></login-form>
+            <login-form @login="logMeIn($event)"
+                        :button-label="Math.random() < 0.5 ? 'Etykieta A' : 'Etykieta B'"></login-form>
         </div>
 
     </div>
--- src/LoginForm.vue
+++ src/LoginForm.vue
@@ -2,12 +2,13 @@
     <div>
         <label>Zaloguj się e-mailem</label>
         <input type="email" v-model="email">
-        <button @click="enter()">Wchodzę</button>
+        <button @click="enter()">{{ buttonLabel }}</button>
     </div>
 </template>
 
 <script>
     export default {
+        props: ['buttonLabel'],
         data() {
             return {
                 email: ''
