f8ad1744598ff142ca91c25638761cb9d53d5dd1
Agnieszka Wegrzyn <wegrzynagnieszka94@gmail.com>
2020-04-25 11:36:12 +0200

    added LoginForm vue

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,44 @@
 <template>
-  <div>
+    <div>
 
+        <div v-if="authenticatedUsername">
+            <h1>System zapisów na zajęcia</h1>
+            <h3>Witaj {{authenticatedUsername}}!
+                <button @click="logMeOut()">Wyloguj</button>
+            </h3>
 
-    <div v-if="authenticatedUsername">
-      <h1>System zapisów na zajęcia</h1>
-      <h3>Witaj {{authenticatedUsername}}!<button @click="logMeOut()">Wyloguj</button></h3>
+        </div>
+
+        <div v-else>
+            <h1>Witaj w systemie zapisów na zajęcia</h1>
+            <login-form @login="logMeIn($event)"></login-form>
+        </div>
 
     </div>
-
-    <div v-else>
-      <h1>Witaj w systemie zapisów na zajęcia</h1>
-      <h3>Zaloguj się e-mailem
-      <input type="email"
-             v-model="email">
-      <button @click="logMeIn()">Wchodzę</button>
-      </h3>
-    </div>
-
-  </div>
 
 </template>
 
 <script>
-import "milligram";
-export default {
-  data(){
-    return{
-     email:'',
-     authenticatedUsername:''
-    };
-  },
-  methods: {
-    logMeIn() {
-      this.authenticatedUsername = this.email;
-      this.email='';
-    },
-    logMeOut(){
-      this.authenticatedUsername='';
+    import "milligram";
+    import LoginForm from "./LoginForm";
+
+    export default {
+        components: {LoginForm},
+        data() {
+            return {
+                email: '',
+                authenticatedUsername: ''
+            };
+        },
+        methods: {
+            logMeIn(username) {
+                this.authenticatedUsername = username;
+            },
+            logMeOut() {
+                this.authenticatedUsername = '';
+            }
+        }
     }
-  }
-}
 </script>
 
 <style>
