3c830a5512d0148d20c9b43692819c81de18c935
Agnieszka Wegrzyn <wegrzynagnieszka94@gmail.com>
2020-04-25 10:58:17 +0200

    login and logout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-    <h1>Twoj adres e-mail to {{email}}</h1>
-    <div v-if="email.length < 10">Ale masz krótki adres!</div>
-    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-    <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
-    <input type="email"
-           v-model="email">
-    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
+    <h1>Witaj w systemie zapisów na zajęcia</h1>
+
+    <div v-if="authenticatedUsername">
+      <h3>Zalogowany jako: {{authenticatedUsername}}</h3>
+      <button @click="logMeOut()">Wyloguj</button>
+    </div>
+
+    <div v-else>
+      <p>Zaloguj się e-mailem
+      <input type="email"
+             v-model="email">
+      <button @click="logMeIn()">Wchodzę</button>
+      </p>
+    </div>
+
+
   </div>
 
 </template>
@@ -15,12 +24,17 @@
 export default {
   data(){
     return{
-     email:''
+     email:'',
+     authenticatedUsername:''
     };
   },
   methods: {
-    alertMyEmail() {
-      alert(this.email);
+    logMeIn() {
+      this.authenticatedUsername = this.email;
+      this.email='';
+    },
+    logMeOut(){
+      this.authenticatedUsername='';
     }
   }
 }
