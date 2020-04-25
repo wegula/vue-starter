96310e53a86ee4b6d665124eea72b52cad37455b
Agnieszka Wegrzyn <wegrzynagnieszka94@gmail.com>
2020-04-25 11:49:28 +0200

    mounted

--- src/LoginForm.vue
+++ src/LoginForm.vue
@@ -2,12 +2,13 @@
     <div>
         <label>Zaloguj się e-mailem</label>
         <input type="email" v-model="email">
-        <button @click="enter()">Wchodzę</button>
+        <button @click="enter()">{{buttonLabel}}</button>
     </div>
 </template>
 
 <script>
     export default {
+        props: ['buttonLabel'],
         data() {
             return {
                 email: ''
@@ -16,6 +17,11 @@
         methods: {
             enter() {
                 this.$emit('login', this.email);
+            }
+        },
+        mounted() {
+            if (!this.buttonLabel) {
+                this.buttonLabel = 'Zaloguj się';
             }
         }
     }
