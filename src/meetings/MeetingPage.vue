4d9027de4bf7d3f509e57db480be16e0b80d83b6
Agnieszka Wegrzyn <wegrzynagnieszka94@gmail.com>
2020-04-25 12:13:15 +0200

    meeting folder and new components

---
+++ src/meetings/MeetingPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>Zajęcia</h2>
+        <new-meeting-form @added="addNewMeeting($event)"></new-meeting-form>
+        <meetings-list :meetings="meetings"></meetings-list>
+    </div>
+</template>
+
+<script>
+    import NewMeetingForm from "./NewMeetingForm";
+    import MeetingsList from "./MeetingsList";
+
+    export default {
+        components: {NewMeetingForm, MeetingsList},
+        data() {
+            return {
+                meetings: []
+            };
+        },
+        methods: {
+            addNewMeeting(meeting) {
+                this.meetings.push(meeting);
+            }
+        }
+    }
+</script>
