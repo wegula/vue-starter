3b84faee7200367e2d9d3a7938d48fa7988d881d
Agnieszka Wegrzyn <wegrzynagnieszka94@gmail.com>
2020-04-25 10:08:52 +0200

    click button

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <h1>Twoj adres e-mail to {{email}}</h1>
-    <input type="email"
-           v-model="email">
     <div v-if="email.length < 10">Ale masz krótki adres!</div>
     <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
     <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+    <input type="email"
+           v-model="email">
+    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
   </div>
 
 </template>
@@ -16,6 +17,11 @@ export default {
     return{
      email:''
     };
+  },
+  methods: {
+    alertMyEmail() {
+      alert(this.email);
+    }
   }
 }
 </script>
